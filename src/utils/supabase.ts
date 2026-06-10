8a66d36797434f324af3d3cbb5a2622cb648987c
Josué Ferreyra <josueferreyra8824@gmail.com>
2026-06-10 19:08:13 -0300

    Agregar configuracion de Supabase

---
+++ src/utils/supabase.ts
@@ -0,0 +1,38 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+import 'expo-sqlite/localStorage/install';
+import { Platform } from 'react-native';
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_KEY!;
+
+console.log('Supabase URL:', supabaseUrl);
+console.log('Supabase Publishable Key:', supabasePublishableKey);
+
+// Manejo para SSR: Si estamos en un entorno sin window, usamos AsyncStorage
+const isWeb = Platform.OS === 'web';
+const customStorage = isWeb
+  ? {
+      getItem: async (key: string) => {
+        if (typeof window === 'undefined') return null;
+        return window.localStorage.getItem(key);
+      },
+      setItem: async (key: string, value: string) => {
+        if (typeof window === 'undefined') return;
+        window.localStorage.setItem(key, value);
+      },
+      removeItem: async (key: string) => {
+        if (typeof window === 'undefined') return;
+        window.localStorage.removeItem(key);
+      },
+    }
+  : AsyncStorage;
+
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+  auth: {
+    storage: customStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
