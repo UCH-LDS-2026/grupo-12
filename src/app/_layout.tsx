e4a95812f1f1652e7db78beb766ed729b280701b
Josué Ferreyra <josueferreyra8824@gmail.com>
2026-06-19 12:32:39 -0300

    Configuración de toasts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,22 @@
 import "@/global.css";
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
+import { Toaster } from 'sonner-native';
 
 export default function RootLayout() {
   return (
-    <>
+    <GestureHandlerRootView className="flex-1">
       <Stack screenOptions={{ headerShown: false }} />
       <StatusBar style="auto" />
-    </>
+      <Toaster
+        duration={3000}
+        position="top-center"
+        theme="light"
+        richColors
+        allowFontScaling
+      />
+    </GestureHandlerRootView>
   );
 }
